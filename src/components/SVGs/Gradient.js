0056fc06363db7114e0d70fa27ba157b2b417394
Zachary Melendez <zacmelendez@gmail.com>
2022-01-29 20:29:33 -0500

    Fixed planetary movement

--- src/components/SVGs/Gradient.js
+++ src/components/SVGs/Gradient.js
@@ -1,30 +1,17 @@
 export default function Gradient({ width, height, refer }) {
     return (
-        // <svg width="896" height="4205" viewBox="0 0 896 4205" fill="none" xmlns="http://www.w3.org/2000/svg" ref={refer}>
-        //     <path d="M0 0H896V4205H0V0Z" fill="url(#paint0_linear_358_410)" />
-        //     <defs>
-        //         <linearGradient id="paint0_linear_358_410" x1="448" y1="0" x2="448" y2="4205" gradientUnits="userSpaceOnUse">
-        //             <stop stopColor="#232126" />
-        //             <stop offset="0.149406" stopColor="#220B40" />
-        //             <stop offset="0.337053" stopColor="#3B5483" />
-        //             <stop offset="0.488095" stopColor="#B5364D" />
-        //             <stop offset="0.670387" stopColor="#C56863" />
-        //             <stop offset="0.879464" stopColor="#79C8D2" />
-        //         </linearGradient>
-        //     </defs>
-        // </svg>
         <svg width="896" height="5622" viewBox="0 0 896 5622" fill="none" xmlns="http://www.w3.org/2000/svg" ref={refer}>
-        <path d="M0 0H896V5622H0V0Z" fill="url(#paint0_linear_358_410)"/>
-        <defs>
-        <linearGradient id="paint0_linear_358_410" x1="448" y1="0" x2="448" y2="5622" gradientUnits="userSpaceOnUse">
-        <stop offset="0.148492" stop-color="#232126"/>
-        <stop offset="0.267984" stop-color="#220B40"/>
-        <stop offset="0.414633" stop-color="#3B5483"/>
-        <stop offset="0.579387" stop-color="#B5364D"/>
-        <stop offset="0.744141" stop-color="#C56863"/>
-        <stop offset="0.879464" stop-color="#79C8D2"/>
-        </linearGradient>
-        </defs>
+            <path d="M0 0H896V5622H0V0Z" fill="url(#paint0_linear_358_410)" />
+            <defs>
+                <linearGradient id="paint0_linear_358_410" x1="448" y1="0" x2="448" y2="5622" gradientUnits="userSpaceOnUse">
+                    <stop offset="0.148492" stopColor="#232126" />
+                    <stop offset="0.267984" stopColor="#220B40" />
+                    <stop offset="0.414633" stopColor="#3B5483" />
+                    <stop offset="0.579387" stopColor="#B5364D" />
+                    <stop offset="0.744141" stopColor="#C56863" />
+                    <stop offset="0.879464" stopColor="#79C8D2" />
+                </linearGradient>
+            </defs>
         </svg>
     )
 }
--- src/components/ScrollDiv/ScrollDiv.js
+++ src/components/ScrollDiv/ScrollDiv.js
@@ -525,16 +525,17 @@ export function Animations({ element, gradRef, treeORef, treeTwRef, treeThRef, w
     gsap.fromTo(
         jupRef.current,
         {
-            y: () => pctToPx(-100),
-            x: () => pctToPx(-100),
+            y: () => pctToPx(-60),
+            x: () => pctToPx(-60),
             width: () => pctToPx(60),
             height: () => pctToPx(60)
         },
         {
             scrollTrigger: {
                 trigger: element.querySelector("#work"),
-                start: "top 100",
+                start: "+=500 1",
                 end: "bottom 100",
+                markers: true,
                 scrub: 2,
                 toggleActions: "restart pause reverse pause"
             },
